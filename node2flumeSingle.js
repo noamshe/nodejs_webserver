737ae156fb846171af084ef69835aba7f1de693d
Noam Shemesh <noam.s@matomy.com>
2014-04-24 11:32:21 +0300

    single cleaned

--- node2flumeSingle.js
+++ node2flumeSingle.js
@@ -1,10 +1,11 @@
 
-
 var sys = require("sys"),
 my_http = require("http");
 var net = require('net');
 var flumeClient = new net.Socket();
-// this is flume tcp socket
+var useCppModule = true;
+
+// This is flume tcp socket
 flumeClient.connect(81, '127.0.0.1', function() {
     console.log('Connected');
 });
@@ -17,56 +18,55 @@ flumeClient.on('close', function() {
 });
 
 my_http.createServer(function(request,response){
+
     //sys.puts("I got kicked");
     handleRequest(request, response);
-
 
 }).listen(8080);
 sys.puts("Server Running on 8080");
 
-
 function handleRequest(request, response) {
-    var qs = require('querystring');
     if (request.method == 'POST') {
         var body = '';
         request.on('data', function (data) {
             body += data;
-            // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
-            if (body.length > 1e6) {
-                // FLOOD ATTACK OR FAULTY CLIENT, NUKE REQUEST
-                request.connection.destroy();
+            if (body.length > 1e6) {// 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
+                request.connection.destroy();// FLOOD ATTACK OR FAULTY CLIENT, NUKE REQUEST
             }
         });
         request.on('end', function () {
-            //console.dir(body);
-
-            var POST = qs.parse(body);
-            // use POST
-            //console.dir(" - " + POST.xml);
-            xmlParser(POST.xml, response);
+            if (useCppModule) {
+                xmlParserCppModule(body, response);
+            } else {
+                xmlParserNodeJSModule(body, response);
+            }
         });
     }
 }
 
-function xmlParser(xml, response) {
-//    var xml = "<root>Hello xml2js!</root>"
-/*
+function xmlParserNodeJSModule(xml, response) {
+    //var xml = "<id>999</id>"
     var parseString = require('xml2js').parseString;
     parseString(xml, function (err, result) {
-        rootElement = JSON.stringify(result['id']);
-        console.dir("result: " + rootElement);
-        flumeClient.write(rootElement + "\n");
+        dataStr = JSON.stringify(result['id']);
+        flumeClient.write(dataStr + "\n", function() {
+            writeResponse(response);
+        });
     });
-*/
-//var addon = require('./build/Release/hello');
-//console.log(addon.hello(xml)); // 'world'
-//flumeClient.write(addon.hello(xml) + "\n", function() {
-//flumeClient.write("noam" + "\n", function() {
-    // write no-bid back
-    response.writeHeader(204, {"Content-Type": "text/plain"});
-    response.write("Hello World");
-    response.end();
-//});
+}
 
+function xmlParserCppModule(xml, response) {
+    var addon = require('./build/Release/hello');
+    var dataStr = addon.hello(xml);
+    flumeClient.write(dataStr + "\n", function() {
+        writeResponse(response);
+    });
+}
+
+
+function writeResponse(response) {
+    response.writeHeader(204, {"Content-Type": "text/plain"});
+    response.write("no-bid");
+    response.end();
 }
 
